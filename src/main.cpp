ebdae3bd6a17e3046caa5d3cc55089261b26885b
star9029 <hengxings783@gmail.com>
2025-06-01 19:06:27 +0800

    feat: support dino

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,35 @@
 #include <string_view>
+#include <format>
 
 #include <windows.h>
 #include <detours.h>
 
+#include "base/basic_type.hpp"
 #include "helpful.hpp"
 #include "ark.hpp"
 
-std::byte* status_string_address1; // L"%.1f / "
-std::byte* status_string_address2; // L"%.1f %%"
-
-static void u8_to_str(u8 value, wchar_t* string) noexcept
-{
-    if (value < 10)
-    {
-        string[0] = ' ';
-        string[1] = ' ';
-        string[2] = '0' + value % 10;
-    }
-    else if (value < 100)
-    {
-        string[0] = ' ';
-        string[1] = '0' + (value / 10) % 10;
-        string[2] = '0' + value % 10;
-    }
-    else
-    {
-        string[0] = '0' + (value / 100) % 10;
-        string[1] = '0' + (value / 10) % 10;
-        string[2] = '0' + value % 10;
-    }
-}
+std::byte* status_string_address1;
+std::byte* status_string_address2;
 
 NAMESPACE_BEGIN(original)
 
 constexpr std::wstring_view status_string1 = L"%.1f / ";
 constexpr std::wstring_view status_string2 = L"%.1f %%";
 
+using fn_IsPrimalDino = bool(__fastcall*)(void* self);
+
 static void(__fastcall* GetStatusValueString)(void* self, void* result, int ValueType, void* bValueOnly);
+static auto const GetPrimalCharacter = reinterpret_cast<void*(__fastcall*)(void* self)>(get_rva(0x5A0BD0));
 
 NAMESPACE_END(original)
 
 NAMESPACE_BEGIN(hook)
 
-constexpr std::wstring_view status_string1 = L"254 | %.1f / ";
-constexpr std::wstring_view status_string2 = L"254 | %.1f %%";
+// constexpr std::wstring_view person_status_string1 = L"254 | %.1f / ";
+// constexpr std::wstring_view person_status_string2 = L"254 | %.1f %%";
+
+constexpr std::wstring_view dino_status_string1 = L"254 + 254 | %.1f / ";
+constexpr std::wstring_view dino_status_string2 = L"254 + 254 | %.1f %%";
 
 void __fastcall GetStatusValueString(void* self, void* result, int ValueType, void* bValueOnly)
 {
@@ -88,17 +74,41 @@ void __fastcall GetStatusValueString(void* self, void* result, int ValueType, vo
     auto const NumberOfLevelUpPointsApplied = reinterpret_cast<u8*>(reinterpret_cast<std::byte*>(self) + 0x138);
     auto const point = NumberOfLevelUpPointsApplied[ValueType];
 
-    if (ValueType < 8)
+    auto const AActor = original::GetPrimalCharacter(self);
+    auto const AActor_vtable_address = *reinterpret_cast<usize*>(AActor);
+    auto const AActor_IsPrimalDino_address = *reinterpret_cast<usize*>(AActor_vtable_address + 0x860);
+    auto const IsPrimalDino = reinterpret_cast<original::fn_IsPrimalDino>(AActor_IsPrimalDino_address);
+    if (IsPrimalDino(AActor))
     {
-        auto str = reinterpret_cast<wchar_t*>(status_string_address1);
-        std::memcpy(status_string_address1, status_string1.data(), (status_string1.size() + 1) * sizeof(wchar_t));
-        u8_to_str(point, str);
+        auto const NumberOfLevelUpPointsAppliedTamed = reinterpret_cast<u8*>(reinterpret_cast<std::byte*>(self) + 0x144);
+        auto const tame_point = NumberOfLevelUpPointsAppliedTamed[ValueType];
+        if (ValueType < 8)
+        {
+            auto str = reinterpret_cast<wchar_t*>(status_string_address1);
+            auto end = std::format_to(str, L"{} + {} | %.1f / ", point, tame_point);
+            *end = '\0';
+        }
+        else
+        {
+            auto str = reinterpret_cast<wchar_t*>(status_string_address2);
+            auto end = std::format_to(str, L"{} + {} | %.1f %%", point, tame_point);
+            *end = '\0';
+        }
     }
     else
     {
-        auto str = reinterpret_cast<wchar_t*>(status_string_address2);
-        std::memcpy(status_string_address2, status_string2.data(), (status_string2.size() + 1) * sizeof(wchar_t));
-        u8_to_str(point, str);
+        if (ValueType < 8)
+        {
+            auto str = reinterpret_cast<wchar_t*>(status_string_address1);
+            auto end = std::format_to(str, L"{} | %.1f / ", point);
+            *end = '\0';
+        }
+        else
+        {
+            auto str = reinterpret_cast<wchar_t*>(status_string_address2);
+            auto end = std::format_to(str, L"{} | %.1f %%", point);
+            *end = '\0';
+        }
     }
 
     original::GetStatusValueString(self, result, ValueType, bValueOnly);
@@ -131,7 +141,7 @@ bool patch()
 {
     {
         auto const target_address = get_rva(0x5A7C09);
-        status_string_address1 = malloc((hook::status_string1.size() + 1) * 2, target_address);
+        status_string_address1 = malloc((hook::dino_status_string1.size() + 1) * 2, target_address);
         if (!status_string_address1)
         {
     #ifdef PROJECT_DEBUG
@@ -150,7 +160,7 @@ bool patch()
 
     {
         auto const target_address = get_rva(0x5A7A53);
-        status_string_address2 = malloc((hook::status_string2.size() + 1) * 2, target_address);
+        status_string_address2 = malloc((hook::dino_status_string2.size() + 1) * 2, target_address);
         if (!status_string_address2)
         {
     #ifdef PROJECT_DEBUG
